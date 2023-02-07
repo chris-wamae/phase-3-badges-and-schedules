a28bf5396630d3de666ff02e8895514549f8e948
chris-wamae <chriswamae123@gmail.com>
2023-02-07 15:30:38 +0300

    Completed Assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker name  
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator names_array
+    names_array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms names_array
+    rooms = []
+    names_array.each_with_index do |name,index|
+       rooms.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+    end
+    rooms
+end
+
+def printer names_array
+   names = batch_badge_creator(names_array)
+   names.each do |name|
+    puts name
+   end
+    statements = assign_rooms(names_array)
+    statements.each do |statement|
+    puts statement
+   end
+end
+
+printer(["John","Doe","Muji"])
